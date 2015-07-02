b1f9e3da61e186117108109688e77ede3057d0a9
Steffen Haase <sh@java-club.de>
2015-07-02 17:14:53 +0200

    mal wieder etwas dran gearbeitet

--- src/test/scala/b_recursion/a_SumNaturalNumbersTest.scala
+++ src/test/scala/b_recursion/a_SumNaturalNumbersTest.scala
@@ -4,12 +4,15 @@ import org.scalatest.FlatSpec
 import scala.annotation.tailrec
 
 /**
-  * Summe der natürlichen Zahlen von 1 bis n
-  */
+ * Summation of positive natural numbers from 1 to maximum (n)
+ *
+ * sumNaturalNumbers(1) = 0 + 1
+ * sumNaturalNumbers(3) = 0 + 1 + 2 + 3 = 6
+ */
 class a_SumNaturalNumbersTest extends FlatSpec {
-  def sumNaturalNumber(maximum: Int) : Long = sumNaturalNumberTailRecursive(maximum)
+  def sumNaturalNumbers(maximum: Int) : Long = sumNaturalNumbersTailRecursive(maximum)
 
-  def sumNaturalNumberImperativ(maximum: Int) : Long = {
+  def sumNaturalNumbersImperativ(maximum: Int) : Long = {
     var result: Long = 0
     for ( i <- 1 to maximum) {
       result += i
@@ -17,36 +20,36 @@ class a_SumNaturalNumbersTest extends FlatSpec {
     result
   }
 
-  def sumNaturalNumberRecurive(maximum: Int) : Long = {
+  def sumNaturalNumbersRecurive(maximum: Int) : Long = {
     if (maximum == 0)
       0l
     else
-      maximum + sumNaturalNumberRecurive(maximum - 1)
+      maximum + sumNaturalNumbersRecurive(maximum - 1)
   }
 
-  def sumNaturalNumberTailRecursive(maximum: Int) : Long = {
+  def sumNaturalNumbersTailRecursive(maximum: Int) : Long = {
     @tailrec
-    def sumNaturalNumberTailRecursive(maximum: Int, acc: Long) : Long = {
+    def sumNaturalNumbersTailRecursive(maximum: Int, acc: Long) : Long = {
       if (maximum == 0)
         acc
       else
-        sumNaturalNumberTailRecursive(maximum - 1, acc + maximum)
+        sumNaturalNumbersTailRecursive(maximum - 1, acc + maximum)
     }
-    sumNaturalNumberTailRecursive(maximum, 0)
+    sumNaturalNumbersTailRecursive(maximum, 0)
   }
 
   "sum of natural numbers from 1 to 3" should "be 6" in {
-    val count = sumNaturalNumber(3)
+    val count = sumNaturalNumbers(3)
     assert(count == 6)
   }
 
   "sum of natural numbers from 1 to 10" should "be 55" in {
-     val count = sumNaturalNumber(55)
+     val count = sumNaturalNumbers(55)
      assert(count == 1540)
    }
 
   "sum of natural numbers from 1 to 100000" should "be 55" in {
-    val count = sumNaturalNumber(100000)
+    val count = sumNaturalNumbers(100000)
     assert(count == 5000050000l)
   }
 }
