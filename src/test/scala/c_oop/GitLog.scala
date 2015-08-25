e8a046254d017a98179aa4ed27199e950b8c4154
Matthias Erche <matthias.erche@epost-dev.de>
2015-08-25 18:09:00 +0200

    Brainstormed over OOP test examples

--- src/test/scala/c_oop/GitLog.scala
+++ src/test/scala/c_oop/GitLog.scala
@@ -1,16 +1,41 @@
 package c_oop
 
+import c_oop.GitLogJsonModel.GitLog
 import play.api.libs.json.Json
 
-case class LogEntry(commit: String)
+object GitLogJsonModel {
 
-object LogEntry {
-  implicit val jsonReads = Json.reads[LogEntry]
+  /**
+   * When working with play applications, case classes are used
+   * to define Data Transfer Objects which hold deserialized Json values.
+   *
+   * The Json properties are represented by case class members,i.e.
+   *
+   * {
+   * "commit": "16bc9b75cec01eccff8448b6a11e968f1a35e7a7"...
+   * }
+   *
+   * is mapped to property "commit"
+   */
+  case class LogEntry(commit: String, author: String)
+
+  /**
+   * Companion objects can be used
+   * to define implicit Json deserializer.
+   */
+  object LogEntry {
+    implicit val jsonReads = Json.reads[LogEntry]
+  }
+
+  case class GitLog(logEntries: Seq[LogEntry])
+
+  object GitLog {
+    implicit val jsonReads = Json.reads[GitLog]
+  }
+
 }
 
-case class GitLog(gitlog: Seq[LogEntry])
-
-
-object GitLog {
-  implicit val jsonReads = Json.reads[GitLog]
+object GitLogService {
+  def commitsByUser(log: GitLog) = log.logEntries.groupBy(_.author)
 }
+
