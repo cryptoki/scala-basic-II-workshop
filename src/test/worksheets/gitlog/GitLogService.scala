c9f259741776b49c170cd60bdd3062f860cba591
Matthias Erche <matthias.erche@epost-dev.de>
2015-09-24 23:42:07 +0200

    Remove example implementations

--- src/test/worksheets/gitlog/GitLogService.scala
+++ src/test/worksheets/gitlog/GitLogService.scala
@@ -13,19 +13,21 @@ object GitLogService {
   /**
    * Count total number of commits
    */
-  def countCommits(log: GitLog): Int = ???
+  def countCommits(log: GitLog): Int = log.logEntries.size
 
   /**
    * Count commits of a specific author. Author is defined like "Matthias Erch <matthias.erche@epost-dev.de"
    */
-  def countCommitsOfUser(log: GitLog, author: String): Int = ???
+  def countCommitsOfUser(log: GitLog, author: String): Int = log.logEntries.count(_.author == author)
 
   /**
    * Count number of contributers.
    *
    * Duplicates caused by multiple mail addresses for the same contributer are allowed.
    */
-  def countOfContributer(log: GitLog): Int = ???
+  def countOfContributer(log: GitLog): Int = {
+    log.logEntries.groupBy(x => x.author).keys.size
+  }
 
   val inputFormatter: DateTimeFormatter = DateTimeFormat.forPattern("EEE MMM dd HH:mm:ss yyyy Z").withLocale(Locale.ENGLISH)
   val outputFormatter: DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd").withLocale(Locale.ENGLISH)
@@ -33,12 +35,22 @@ object GitLogService {
   /**
    * Use [[inputFormatter]] and [[outputFormatter]] to get a [[Map]] of number of commits per day
    */
-  def logEntryPerDay(log: GitLog): Map[String, Int] = ???
+  def logEntryPerDay(log: GitLog): Map[String, Int] = {
+    log.logEntries.groupBy(x => {
+      val jodatime: DateTime = inputFormatter.parseDateTime(x.date)
+      outputFormatter.print(jodatime)
+    }).map(x => (x._1, x._2.size))
+  }
 
   /**
    * Return a map of log entries per author name
    */
-  def logEntryByName(log: GitLog): Map[String, Seq[LogEntry]] = ???
+  def logEntryByName(log: GitLog): Map[String, Seq[LogEntry]] = {
+    log.logEntries.groupBy(x => {
+      val author = authorFromLogEntry(x)
+      author.name
+    })
+  }
 
   /**
    * Hints:
@@ -47,17 +59,22 @@ object GitLogService {
    * - arrays support mkString to join ll elements to a string
    *
    */
-  def authorFromLogEntry(logEntry: LogEntry): Author = ???
+  def authorFromLogEntry(logEntry: LogEntry): Author = {
+    val splitted = logEntry.author.split(" ")
+    val nameParts = splitted.takeWhile(!_.startsWith("<"))
+    val email = splitted.dropWhile(!_.startsWith("<"))
+    new Author(nameParts.mkString(" "), email(0))
+  }
 
   /**
    * Transform a list of [[LogEntry]] into a list of [[Author]]
    */
-  def authorFromLogEntries(logEntries: Seq[LogEntry]): Seq[Author] = ???
+  def authorFromLogEntries(logEntries: Seq[LogEntry]): Seq[Author] = logEntries.map(authorFromLogEntry)
 
   /**
    * Get a specific author from a list of authors
    */
-  def author(authors: Seq[Author], name: String): Author = ???
+  def author(authors: Seq[Author], name: String): Author = authors.find(_.name == name).get
 
 
 }
